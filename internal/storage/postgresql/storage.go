0766ac12f09dd3d83edcdd3e9fe4a9d68e4debe9
Sean McGary <sean@seanmcgary.com>
2024-09-06 10:41:47 -0500

    Remove dead code

--- internal/storage/postgresql/storage.go
+++ internal/storage/postgresql/storage.go
@@ -42,10 +42,6 @@ func NewPostgresBlockStore(db *gorm.DB, cfg *config.Config, l *zap.Logger) (*Pos
 	return mds, nil
 }
 
-func (m *PostgresBlockStore) GetDb() *gorm.DB {
-	return m.Db
-}
-
 func (m *PostgresBlockStore) autoMigrate() {
 	if m.migrated {
 		return
@@ -59,36 +55,6 @@ func (m *PostgresBlockStore) CreateTxBlock(tx *gorm.DB) *gorm.DB {
 		return tx
 	}
 	return m.Db.Begin()
-}
-
-func (m *PostgresBlockStore) GetNextSequenceId() (uint64, error) {
-	return pg.WrapTxAndCommit[uint64](func(txn *gorm.DB) (uint64, error) {
-		query := `SELECT coalesce(max(id), -1) + 1 FROM block_sequences`
-
-		var nextId uint64
-		result := txn.Raw(query).Scan(&nextId)
-
-		if result.Error != nil {
-			return 0, xerrors.Errorf("Failed to get next sequence id: %w", result.Error)
-		}
-
-		return nextId, nil
-	}, nil, m.Db)
-}
-
-func (m *PostgresBlockStore) UpdateBlockPath(sequenceId uint64, blockNumber uint64, path string) (*storage.Block, error) {
-	return pg.WrapTxAndCommit[*storage.Block](func(txn *gorm.DB) (*storage.Block, error) {
-		sequence := &storage.Block{}
-		result := txn.Model(sequence).
-			Clauses(clause.Returning{}).
-			Where("id = ? and number = ?", sequenceId, blockNumber).
-			Update("blob_path", path)
-
-		if result.Error != nil {
-			return nil, xerrors.Errorf("Failed to update block path: %w", result.Error)
-		}
-		return sequence, nil
-	}, nil, m.Db)
 }
 
 func (m *PostgresBlockStore) InsertBlockAtHeight(
@@ -147,38 +113,6 @@ func (m *PostgresBlockStore) InsertBlockTransaction(
 	}, nil, m.Db)
 }
 
-func (m *PostgresBlockStore) BatchInsertBlockTransactions(
-	sequenceId uint64,
-	blockNumber uint64,
-	transactions []storage.BatchTransaction,
-) ([]*storage.Transaction, error) {
-	if len(transactions) == 0 {
-		return make([]*storage.Transaction, 0), nil
-	}
-	return pg.WrapTxAndCommit[[]*storage.Transaction](func(txn *gorm.DB) ([]*storage.Transaction, error) {
-		txs := make([]*storage.Transaction, 0, len(transactions))
-		for _, tx := range transactions {
-			txs = append(txs, &storage.Transaction{
-				BlockSequenceId:  sequenceId,
-				BlockNumber:      blockNumber,
-				TransactionHash:  tx.TxHash,
-				TransactionIndex: tx.TxIndex,
-				FromAddress:      tx.From,
-				ToAddress:        tx.To,
-				ContractAddress:  tx.ContractAddress,
-				BytecodeHash:     tx.BytecodeHash,
-			})
-		}
-
-		result := m.Db.Model(&storage.Transaction{}).Clauses(clause.Returning{}).Create(&txs)
-
-		if result.Error != nil {
-			return nil, xerrors.Errorf("Failed to insert block transaction: %w", result.Error)
-		}
-		return txs, nil
-	}, nil, m.Db)
-}
-
 func (m *PostgresBlockStore) InsertTransactionLog(
 	txHash string,
 	transactionIndex uint64,
@@ -219,36 +153,6 @@ func (m *PostgresBlockStore) InsertTransactionLog(
 	}, nil, m.Db)
 }
 
-func (m *PostgresBlockStore) BatchInsertTransactionLogs(transactions []*storage.BatchInsertTransactionLogs) ([]*storage.TransactionLog, error) {
-	logs := make([]*storage.TransactionLog, 0)
-
-	for _, tx := range transactions {
-		for _, log := range tx.ParsedTransaction.Logs {
-			argsJson, err := json.Marshal(log.Arguments)
-			if err != nil {
-				m.Logger.Sugar().Errorw("Failed to marshal arguments", zap.Error(err))
-			}
-			logs = append(logs, &storage.TransactionLog{
-				TransactionHash:  tx.Transaction.Hash.Value(),
-				TransactionIndex: tx.Transaction.Index.Value(),
-				BlockNumber:      tx.Transaction.BlockNumber.Value(),
-				BlockSequenceId:  0,
-				Address:          strings.ToLower(log.Address),
-				Arguments:        string(argsJson),
-				EventName:        log.EventName,
-				LogIndex:         log.LogIndex,
-			})
-		}
-	}
-
-	result := m.Db.Model(&storage.TransactionLog{}).Clauses(clause.Returning{}).Create(&logs)
-
-	if result.Error != nil {
-		return nil, xerrors.Errorf("Failed to insert block transaction: %w", result.Error)
-	}
-	return logs, nil
-}
-
 type latestBlockNumber struct {
 	BlockNumber uint64
 }
@@ -276,53 +180,6 @@ func (m *PostgresBlockStore) GetBlockByNumber(blockNumber uint64) (*storage.Bloc
 		return nil, result.Error
 	}
 	return block, nil
-}
-
-func (m *PostgresBlockStore) DeleteTransactionLogsForBlock(blockNumber uint64) error {
-	result := m.Db.Where("block_number = ?", blockNumber).Delete(&storage.TransactionLog{})
-	if result.Error != nil {
-		return xerrors.Errorf("Failed to delete transaction logs: %w", result.Error)
-	}
-	return nil
-}
-
-func (m *PostgresBlockStore) GetTransactionByHash(txHash string) (*storage.Transaction, error) {
-	tx := &storage.Transaction{}
-
-	result := m.Db.Model(&tx).Where("transaction_hash = ?", strings.ToLower(txHash)).First(&tx)
-	if result.Error != nil {
-		if errors.Is(result.Error, gorm.ErrRecordNotFound) {
-			return nil, nil
-		}
-		return nil, result.Error
-	}
-	return tx, nil
-}
-
-func (m *PostgresBlockStore) ListTransactionsForContractAddress(contractAddress string) ([]*storage.Transaction, error) {
-	contractAddress = strings.ToLower(contractAddress)
-	var txs []*storage.Transaction
-
-	result := m.Db.Model(&storage.Transaction{}).
-		Where("contract_address = ? or to_address = ?", contractAddress, contractAddress).
-		Find(&txs)
-	if result.Error != nil {
-		return nil, xerrors.Errorf("Failed to list transactions for contract address: %w", result.Error)
-	}
-	return txs, nil
-}
-
-func (m *PostgresBlockStore) ListTransactionLogsForContractAddress(contractAddress string) ([]*storage.TransactionLog, error) {
-	contractAddress = strings.ToLower(contractAddress)
-	var txLogs []*storage.TransactionLog
-
-	result := m.Db.Model(&storage.TransactionLog{}).
-		Where("address = ?", contractAddress).
-		Find(&txLogs)
-	if result.Error != nil {
-		return nil, xerrors.Errorf("Failed to list transaction logs for contract address: %w", result.Error)
-	}
-	return txLogs, nil
 }
 
 func (m *PostgresBlockStore) InsertOperatorRestakedStrategies(avsDirectorAddress string, blockNumber uint64, blockTime time.Time, operator string, avs string, strategy string) (*storage.OperatorRestakedStrategies, error) {
