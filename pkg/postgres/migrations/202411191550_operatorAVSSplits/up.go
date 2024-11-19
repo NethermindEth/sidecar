900b4952866f64f3026652f66c9e560532f2ecf2
0xrajath <rajathalex@gmail.com>
2024-11-19 15:57:48 -0500

    migration

---
+++ pkg/postgres/migrations/202411191550_operatorAVSSplits/up.go
@@ -0,0 +1,34 @@
+package _202411191550_operatorAVSSplits
+
+import (
+	"database/sql"
+
+	"gorm.io/gorm"
+)
+
+type Migration struct {
+}
+
+func (m *Migration) Up(db *sql.DB, grm *gorm.DB) error {
+	query := `
+		create table if not exists operator_avs_splits (
+			operator varchar not null,
+			avs varchar not null,
+			activated_at timestamp(6) not null,
+			old_operator_avs_split_bips integer not null,
+			new_operator_avs_split_bips integer not null,
+			block_number bigint not null,
+			transaction_hash varchar not null,
+			log_index bigint not null,
+			unique(transaction_hash, log_index, block_number)
+		);
+	`
+	if err := grm.Exec(query).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+func (m *Migration) GetName() string {
+	return "202411191550_operatorAVSSplits"
+}
--- pkg/postgres/migrations/migrator.go
+++ pkg/postgres/migrations/migrator.go
@@ -34,6 +34,7 @@ import (
 	_202411130953_addHashColumns "github.com/Layr-Labs/sidecar/pkg/postgres/migrations/202411130953_addHashColumns"
 	_202411131200_eigenStateModelConstraints "github.com/Layr-Labs/sidecar/pkg/postgres/migrations/202411131200_eigenStateModelConstraints"
 	_202411151931_operatorDirectedRewardSubmissions "github.com/Layr-Labs/sidecar/pkg/postgres/migrations/202411151931_operatorDirectedRewardSubmissions"
+	_202411191550_operatorAVSSplits "github.com/Layr-Labs/sidecar/pkg/postgres/migrations/202411191550_operatorAVSSplits"
 	_202411191947_cleanupUnusedTables "github.com/Layr-Labs/sidecar/pkg/postgres/migrations/202411191947_cleanupUnusedTables"
 	_202412021311_stakerOperatorTables "github.com/Layr-Labs/sidecar/pkg/postgres/migrations/202412021311_stakerOperatorTables"
 	_202412061553_addBlockNumberIndexes "github.com/Layr-Labs/sidecar/pkg/postgres/migrations/202412061553_addBlockNumberIndexes"
@@ -112,6 +113,7 @@ func (m *Migrator) MigrateAll() error {
 		&_202411130953_addHashColumns.Migration{},
 		&_202411131200_eigenStateModelConstraints.Migration{},
 		&_202411151931_operatorDirectedRewardSubmissions.Migration{},
+		&_202411191550_operatorAVSSplits.Migration{},
 		&_202411191947_cleanupUnusedTables.Migration{},
 		&_202412021311_stakerOperatorTables.Migration{},
 		&_202412061553_addBlockNumberIndexes.Migration{},
