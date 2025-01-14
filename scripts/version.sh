c8c540ddac6637532156e931870ee34d00716be9
Sean McGary <sean@seanmcgary.com>
2025-01-14 17:01:19 -0600

    fix: tweak version script that was returning an empty string

--- scripts/version.sh
+++ scripts/version.sh
@@ -3,6 +3,7 @@
 REF=$1
 
 versionFile=$(cat VERSION | tr -d '[:space:]')
+echo "Using version '${versionFile}'"
 if [[ $REF == refs/tags/* ]]; then
     # check if versionFile equals the tag.
     if [[ $versionFile != "${REF#refs/tags/}" ]]; then
@@ -13,7 +14,7 @@ if [[ $REF == refs/tags/* ]]; then
 else
     # if this isnt a release, add the commit hash to the end of the version
     v=$(git rev-parse --short HEAD)
-    updatedVersion = "${versionFile}+${v}"
+    updatedVersion="${versionFile}+${v}"
     echo "Updated version to '${updatedVersion}'"
     echo -n $updatedVersion > VERSION
 fi
