153712575378864ff6905b76c20443f404a99a29
Sean McGary <sean@seanmcgary.com>
2025-02-25 13:46:16 -0600

    fix the linter

--- pkg/eigenState/operatorDirectedRewardSubmissions/operatorDirectedRewardSubmissions_test.go
+++ pkg/eigenState/operatorDirectedRewardSubmissions/operatorDirectedRewardSubmissions_test.go
@@ -174,6 +174,7 @@ func Test_OperatorDirectedRewardSubmissions(t *testing.T) {
 	t.Run("Ensure an operator directed reward submission with a duration of 0 is not saved", func(t *testing.T) {
 		esm := stateManager.NewEigenStateManager(l, grm)
 		model, err := NewOperatorDirectedRewardSubmissionsModel(esm, grm, l, cfg)
+		assert.Nil(t, err)
 
 		blockNumber := uint64(102)
 
